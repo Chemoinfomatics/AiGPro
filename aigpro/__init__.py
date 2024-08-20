8e2a49582c534329016b3a42ddfa415d1d119dee
Takshan <mrbrahma.rahul@gmail.com>
2024-08-20 16:18:43 +0900

    upload fix

--- aigpro/__init__.py
+++ aigpro/__init__.py
@@ -4,5 +4,5 @@ import logging
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __author__ = "Rahul Brahma"
-__email__ = "mrbrahma.rahul@gmail.com"
+__email__ = "rahul@drugonix.com"
 __version__ = "0.0.1"
